77950530e0dd24c1c2c6827e6b5dae60f03f4e90
Devansh <97612790+hitdevansh@users.noreply.github.com>
2024-06-29 16:47:24 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+python3 manage.py collectstatic
+deactivate
+
